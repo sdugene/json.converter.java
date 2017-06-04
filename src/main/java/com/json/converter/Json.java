87f5da855015fe4f83fa5f21d2b618f1920b92b9
Sébastien Dugène <sebastien@siteoffice.fr>
2017-06-04 17:34:29 +0200

    SO > Json > first commit

---
+++ src/main/java/com/json/converter/Json.java
@@ -0,0 +1,13 @@
+package com.json.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Json
+{
+    public static List<Object> jsonDecode(String Json, Object object)
+    {
+        List<Object> list = new ArrayList<Object>();
+        return list;
+    }
+}
---
+++ src/test/java/com/json.converter/JsonTests.java
@@ -0,0 +1,9 @@
+package com.json.converter;
+
+public class JsonTests {
+
+	public static void main(String [] args) {
+	    System.out.println("test");
+	}
+
+}
