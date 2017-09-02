b65ee85b8ae0ff25b254bbb8a3a4d47b163f3d61
Sebastien Dugene <sebastien.dugene@gmail.com>
2017-09-02 18:22:17 +0200

    SO > sonarqube > clean code

--- src/main/java/com/json/converter/Json.java
+++ src/main/java/com/json/converter/Json.java
@@ -6,8 +6,6 @@ import com.rits.cloning.Cloner;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import static org.apache.commons.beanutils.BeanUtils.populate;
 
 public class Json
 {
