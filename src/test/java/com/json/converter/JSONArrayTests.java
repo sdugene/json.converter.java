2f25ee475ec166b9c0a24f483c97402eac1d16f0
Sebastien Dugene <sebastien.dugene@gmail.com>
2017-09-02 14:13:26 +0200

    SO > jenkins > update dependencies

---
+++ src/test/java/com/json/converter/JSONArrayTests.java
@@ -0,0 +1,14 @@
+package com.json.converter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JSONArrayTests
+{
+    @Test
+    public void instantiateTest()
+    {
+        assertEquals(true, false);
+    }
+}
